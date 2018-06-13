1493eee1083ad823d8ad347f9b36dae6bca8806d
droid1799 <40224436+droid1799@users.noreply.github.com>
2018-06-13 22:04:18 +0100

    Add files via upload

---
+++ droid.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+<head>
+<body>
+<form action="droid.php" method="post">
+<input type="text" name="search">
+<input type="submit" value="submit">
+</form>
+<?php
+if (isset($_post['submit'])){
+$search=$_post['$search'];
+if ($search==$_post['$search']{
+ (location:'https://www.google.dz/search?q=$search')
+ }
+    }
+    ?>
+    </body>
+</html>
